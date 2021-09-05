e07f94f4a2fbc464d45d89a498b2d0a3b032a2ec
prazwal malakar <its.prazwal@gmail.com>
2021-09-05 14:02:04 +0545

    setup completed for production with heroku backed

--- src/Utils/httpRequest/sendRequest.ts
+++ src/Utils/httpRequest/sendRequest.ts
@@ -5,7 +5,7 @@ import { tokenAssembler } from '../utilFunctions'
 const getRootUrl = ():string => {
   const port = process.env.REACT_APP_API_PORT || 8000
   const dev = process.env.REACT_APP_NODE_ENV !== 'production'
-  return dev ? `http://localhost:${port}/api` : 'https://stark-fjord-70567.herokuapp.com'
+  return dev ? `http://localhost:${port}/api` : 'https://stark-fjord-70567.herokuapp.com/api'
 }
 
 const sendRequest = async (
